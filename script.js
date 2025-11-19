a1104db777aa93077f27cdcb97b221c83de81742
Pedro Wilson Nunes Silva <73663746+boson-de-higs@users.noreply.github.com>
2025-11-19 16:18:35 -0300

    Alterando Carrossel 60% e login pra 40%

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-const bgCarousel = document.querySelector(".bg-carousel");
+const img = document.querySelector(".carousel-img");
 
 const images = [
-    "https://images.unsplash.com/photo-1511527844068-006b95d162c2?q=80&w=1170&auto=format&fit=crop",
+    "https://images.unsplash.com/photo-1555215695-3004980ad54e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1720437009715-8a59b6b51146?q=80&w=1170&auto=format&fit=crop",
-    "https://images.unsplash.com/photo-1634737581963-5a22ba471961?q=80&w=943&auto=format&fit=crop",
+    "https://images.unsplash.com/photo-1503376780353-7e6692767b70",
     "https://images.unsplash.com/photo-1605330757549-67b5021634ba?q=80&w=961&auto=format&fit=crop",
-    "https://cdn.motor1.com/images/mgl/xqowy2/s1/chevrolet-onix-plus-premier-2023-vs.-hyundai-hb20s-platinum-plus-2023.webp"
+    "https://blogseminovos.localiza.com/wp-content/uploads/2025/08/Chevrolet-Tracker-RS-1-1024x683.jpg",
 ];
+
 
 let index = 0;
 
-function updateBackground() {
-    bgCarousel.style.backgroundImage = `url('${images[index]}')`;
-    index = (index + 1) % images.length;
+function updateImage() {
+    img.style.opacity = 0;
+
+    setTimeout(() => {
+        img.src = images[index];
+        img.style.opacity = 1;
+        index = (index + 1) % images.length;
+    }, 400);
 }
 
-updateBackground();             
-setInterval(updateBackground, 8000); 
+updateImage();
+setInterval(updateImage, 8000);
